ed02ce26225b4da3d7d10211ed589611ae7cd0b6
thunderndwind <bmaher.ca@gmail.com>
2023-08-18 23:37:44 +0300

    first

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,6 +2,7 @@
 /**
 * print_number - Prints a number
 * @n: The number to print
+*
 */
 void print_number(int n)
 {
@@ -14,9 +15,6 @@ num = -num;
 if (num > 9)
 {
 print_number(num / 10);
-/**
-*recursion
-*/
 }
 _putchar(num % 10 + '0');
 }
