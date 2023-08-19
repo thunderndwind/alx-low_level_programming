01aa552c6184f2456efdc866f13345cc8af6c231
thunderndwind <bmaher.ca@gmail.com>
2023-08-19 05:58:12 +0300

    first

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - prints all numbers from 0 to 9 using _putchar()
+*
+*Return: returns nothing
+*/
+
+void print_numbers(void)
+{
+int num = 48;
+for (num = 48; num < 58; num++)
+{
+_putchar(num);
+}
+_putchar(10);
+}
