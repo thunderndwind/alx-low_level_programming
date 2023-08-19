583c7b9eea8dd982f1725eb9921e9a64faf3101d
thunderndwind <bmaher.ca@gmail.com>
2023-08-19 06:06:31 +0300

    first

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_most_numbers - prints the digits without 2 and 4
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+int num;
+for (num = 48; num < 58; num++)
+{
+if ((num == 50) || (num == 52))
+continue;
+
+_putchar(num);
+}
+_putchar(10);
+}
