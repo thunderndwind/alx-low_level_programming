7ba94c23d7bd3a7eb99946787189aed6cee4d6b0
thunderndwind <bmaher.ca@gmail.com>
2023-08-19 05:46:13 +0300

    first

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: numbers of lines.
+ * Return: no return.
+ */
+ 
+void print_triangle(int size)
+{
+	int rows, columns, x;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (rows = 0; rows < size; rows++)
+	{
+		for (columns = size - rows; columns > 1; columns--)
+		{
+			_putchar(' ');
+		}
+		for (x = rows + columns; x >= 1; x--)
+		{
+			_putchar(35);
+		}
+			_putchar(10);
+	}
+}
