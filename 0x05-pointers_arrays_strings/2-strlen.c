304ecc7ee157453e21452f4067661ff5c0682d72
thunderndwind <bmaher.ca@gmail.com>
2023-08-23 01:06:27 +0300

    super

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: string
+ * Return: returns the lenght of a string;
+ */
+
+int _strlen(char *s)
+{
+	int counter, i;
+	i = 0;
+
+	for (counter = 0; s[counter] != '\0'; counter++)
+		i++;
+
+	return (i);
+}
