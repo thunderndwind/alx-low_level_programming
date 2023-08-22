419272562d3518d8193ca05101b184f731075612
thunderndwind <bmaher.ca@gmail.com>
2023-08-23 01:20:46 +0300

    super

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcpy - Copying from the source and pasting to the destination string
+* @dest: the destination
+* @src: the source
+* Return: the destination pasted string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+	*(dest + i) = *(src + i);
+	i++;
+	}
+	*(dest + i) = '\0';
+	
+	return (dest);
+}
