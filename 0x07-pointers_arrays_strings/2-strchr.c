1fb507bf753b7add04bf1e961cde805bdf2ac376
thunderndwind <bmaher.ca@gmail.com>
2023-08-29 04:11:17 +0300

    super

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - it finds the first occurrence
+ * of the character c in the string s if exists 
+ *
+ * @s:string targeted
+ * @c:character targeted
+ *
+ * Return: returns pointer to first occcurence of c
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0' ; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
