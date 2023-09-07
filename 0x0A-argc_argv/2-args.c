920d9fb81bd1ea227d4817dec5eafc043106890f
thunderndwind <bmaher.ca@gmail.com>
2023-09-07 04:07:23 +0300

    super

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - prints all arguments it received
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: returns 0
+*/
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while(argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+		return (0);
+}
