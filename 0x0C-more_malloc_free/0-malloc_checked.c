269bd44cd292238a905bf0c90fca8a8ef59ae03a
thunderndwind <bmaher.ca@gmail.com>
2023-09-09 04:05:32 +0300

    super

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,17 +3,18 @@
 /**
  * malloc_checked - allocates memory using malloc, exit(98) if it fails
  *
- *@b: size of the memory block to be allocated
+ * @b: size of the memory block to be allocated
  *
  * Return: pointer to the address of the memory block
  */
  
 void *malloc_checked(unsigned int b)
 {
-	void *block;
+	void *mblock;
 
-	block = malloc(b);
-	if (block == NULL)
+	mblock = malloc(b);
+	if (mblock == NULL)
 		exit(98);
-	return (block);
+		
+	return (mblock);
 }
